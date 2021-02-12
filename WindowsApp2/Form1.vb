f370204bec8f6ae7bc063ed7c90d2ccf9b57e10e
Abdurrochman25 <m.abdurrochman9@gmail.com>
2021-02-12 01:55:31 -0800

    add button2

--- WindowsApp2/Form1.vb
+++ WindowsApp2/Form1.vb
@@ -1,12 +1,16 @@
 ﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+
 
 <<<<<<< abdur
 =======
-
-
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         MessageBox.Show("Vik")
 >>>>>>> master
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
     End Sub
 End Class
